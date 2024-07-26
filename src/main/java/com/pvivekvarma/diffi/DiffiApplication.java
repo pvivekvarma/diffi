fa0d4db56ac58672fd6ef7a5efd07a8317971b13
sprp120295 <saiprakash.reddy12@gmail.com>
2024-07-25 21:26:40 -0400

    initial commit

---
+++ src/main/java/com/pvivekvarma/diffi/DiffiApplication.java
@@ -0,0 +1,13 @@
+package com.pvivekvarma.diffi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiffiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiffiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvivekvarma/diffi/DiffiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvivekvarma.diffi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiffiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
